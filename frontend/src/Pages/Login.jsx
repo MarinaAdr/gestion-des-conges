e0edd3de49514cce6d4618adb3d839692959a5b1
Claudia <marinaclaudia003@gmail.com>
2024-10-13 18:29:01 +0300

    creation style login

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,28 +1,24 @@
-import React, { useState } from 'react';
-import {MdEmail} from 'react-icons/md';
-import {RiEyeCloseFill, RiLockPasswordFill} from 'react-icons/ri';
-import LoginImage from '../assets/login.png';
-
+import React, { useState } from "react";
+import { MdEmail } from "react-icons/md";
+import { RiEyeCloseFill, RiLockPasswordFill } from "react-icons/ri";
+import LoginImage from "../assets/login.gif";
 
 const Login = () => {
-  
   return (
-    <div className="flex flex-col items-center h-screen justify-center bg-[#fff] ">
-      <h2 className="font-algerian text-3xl text-[#F5BCBA] ">
-        Gestion des congés
-      </h2>
-
-      <div className="border shadow p-7 w-100 h-90 bg-white">
-        <div className="flex flex-col items-center justify-center">
-          <img src={LoginImage} alt="Login image" className="h-20 " />
+    <div className="flex flex-col items-center h-screen justify-center bg-gradient-to-b from-cyan-400 from-9% to-gray-100 to-50% space-y-6  ">
+      <div className="border shadow p-7  h-90 bg-white  w-[500px] rounded-[10px]">
+        <div className="flex flex-col items-center justify-center ">
+          <img src={LoginImage} alt="Login image" className="h-30 w-40 " />
         </div>
 
-        <h2 className="text-2xl font-bold mb-4 text-center">Connexion</h2>
+        <h2 className="text-2xl font-pacific mb-4 text-center">Connexion</h2>
         <form className="m-5">
-          <div className="mb-4">
+          <div className="mb-4 ">
             <div className="flex items-center gap-2">
               <MdEmail className="text-lg" />
-              <label htmlFor="email" className="">Email</label>
+              <label htmlFor="email" className="text-lg">
+                Email
+              </label>
             </div>
             <input
               type="email"
@@ -32,8 +28,8 @@ const Login = () => {
           </div>
           <div className="mb-4">
             <div className="flex items-center gap-2">
-              <RiLockPasswordFill className='text-lg' />
-              <label htmlFor="mdp" className="">
+              <RiLockPasswordFill className="text-lg" />
+              <label htmlFor="mdp" className="text-lg">
                 Mot de passe
               </label>
             </div>
@@ -46,22 +42,24 @@ const Login = () => {
           <div className="mb-4 flex items-center justify-between gap-4">
             <label className="inline-flex items-center">
               <input type="checkbox" className="form-checkbox" />
-              <span className="ml-2 text-gray-800">Se souvenir de moi</span>
+              <span className="ml-2 text-gray-800 text-lg">
+                Se souvenir de moi
+              </span>
             </label>
-            <a href="#" className="text-[#F5BCBA]"> Mot de passe oublié</a>
+            <a href="#" className="text-[#3bb8d8] texy-lg">
+              {" "}
+              Mot de passe oublié
+            </a>
           </div>
           <div className="mb-4">
             <button
               type="submit"
-              className="w-full bg-[#F5BCBA] text-white py-2"
+              className="w-full bg-[#3bb8d8] text-white py-2"
             >
               Se connecter
             </button>
-
           </div>
-
         </form>
-
       </div>
     </div>
   );
