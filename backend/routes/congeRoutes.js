44668a2371a25332e419285f2807da95e15ef3e7
Claudia <marinaclaudia003@gmail.com>
2024-12-03 15:45:43 +0300

    routes pour récupérer les conges par status

--- backend/routes/congeRoutes.js
+++ backend/routes/congeRoutes.js
@@ -1,20 +1,26 @@
-const express = require('express');
-const router = express.Router();
-const congeController = require('../controllers/congeController');
+const express = require ('express');
+const router = express.Router ();
+const congeController = require ('../controllers/congeController');
 
 // Modifier la route pour correspondre à l'appel du frontend
-router.post('/', congeController.createConge);
+router.post ('/', congeController.createConge);
 
 // Route pour obtenir tous les congés
-router.get('/', congeController.getAllConges);
+router.get ('/', congeController.getAllConges);
 
 // Route pour obtenir les congés d'un utilisateur spécifique
-router.get('/user/:userId', congeController.getCongesByUser);
+router.get ('/user/:userId', congeController.getCongesByUser);
 
 // Route pour mettre à jour le statut d'un congé
-router.patch('/:congeId/status', congeController.updateCongeStatus);
+router.patch ('/:congeId/status', congeController.updateCongeStatus);
 
 // Nouvelle route pour obtenir les statuts possibles
-router.get('/statuts', congeController.getStatuts);
+router.get ('/statuts', congeController.getStatuts);
+
+// Route pour obtenir les congés d'un utilisateur spécifique par statut
+router.get (
+  '/user/:userId/statut/:statut',
+  congeController.getCongesByUserAndStatus
+);
 
 module.exports = router;
